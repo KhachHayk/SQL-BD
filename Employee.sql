b5c860931e01c8f0ddfa5c7fe4ca33103f49ec51
Hayk <khachike@gmail.com>
2023-07-28 21:43:46 +0700

    Initial commit

---
+++ Employee.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS Department (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Employees (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Boss (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS Еmployee (
+	employees_id INTEGER NOT NULL REFERENCES Employees(id),
+	department_id INTEGER NOT NULL REFERENCES Department(id),
+	boss_id INTEGER REFERENCES Boss(id),		
+	CONSTRAINT pk PRIMARY KEY (employees_id, department_id, boss_id)
+);
